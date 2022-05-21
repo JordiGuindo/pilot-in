cde4791bc9afeeec78639fb77c03c645a337cd1b
franmuline <franm2000@gmail.com>
2022-05-21 16:38:53 +0200

    Distance sensor integrated + modifications motor control

--- main.py
+++ main.py
@@ -52,12 +52,24 @@ if __name__ == "__main__":
     GPIO.setup(trigger, GPIO.OUT)
     GPIO.setup(echo, GPIO.IN)
     
-    # This timer is used for several things
-    timer = time.time()
     
     # This variable is used to ensure the ball is in a correct position
     # for some iterations to pick it up
     counter_ball = 0
+    
+    # These variables are used to make the robot search for balls in different locations
+    # If searching is False, the robot will be rotating. If it is True, it will be going forward.
+    # We will also use it to make use of the distance sensor when going forward,
+    # to avoid crashing
+    searching = False
+    # Counter_distance will be used for counting iterations. If the distance is close during
+    # several iterations, the robot will stop and will rotate
+    counter_distance = 0
+    # Timer will help with changing the variable searching. If the robot does not find a ball
+    # it will be rotating for 20 s and going forward for 3 s in a loop, unless the distance
+    # sensor says otherwise
+    t = 20
+    t0 = time.time()
     
 
     while True:
@@ -66,14 +78,58 @@ if __name__ == "__main__":
         if frame is None:
             break
         x, y, r = bd.detect_ball(frame)
-        print(x, y)
         # We show the frame with the ball detected (if there is one)
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
+        if x != -1:
+            t0 = time.time()
         
-        # This function tells the wheel motors how to move to center
-        # the ball
-        c = mc.center_ball(x, pwm_1, pwm_2, m1, m2)
+        # This part of the code controls when the robot has to change between
+        # rotating and going forward if the ball is not found
+        t1 = time.time() - t0
+        if t1 > t and x == -1:
+            if not searching:
+                # If the robot has to go forward, we ensure that there is not an obstacle in front
+                # of it at least in a distance of 1 m
+                pwm_1.ChangeDutyCycle(0)
+                pwm_2.ChangeDutyCycle(0)
+                time.sleep(0.2)
+                d1 = ds.distance(trigger, echo)
+                time.sleep(0.1)
+                d2 = ds.distance(trigger, echo)
+                time.sleep(0.1)
+                if d1 > 60 and d2 > 60:
+                    print("There is not an in front object. Going forward")
+                    t = 15
+                    searching = True
+                    t0 = time.time()
+                else:
+                    # If there is an obstacle, we keep rotating for 5 more seconds
+                    print("There is an obstacle. Keep rotating")
+                    t0 = time.time() - 15
+            else:
+                print("Let's look for a ball in this position")
+                t = 20
+                searching = False
+                t0 = time.time()
+            
+        # This function tells the wheel motors how to move with the purpose of centering the ball
+        # so the robot can take it. It also tells them how to move if there is not a ball
+        c = mc.center_ball(x, pwm_1, pwm_2, m1, m2, searching)
+        
+        if searching and x == -1:
+            d = ds.distance(trigger, echo)
+            time.sleep(0.07)
+            if d < 70:
+                counter_distance += 1
+            if counter_distance > 5:
+                print("Object too close. Start rotating")
+                t = 20
+                searching = False
+                t0 = time.time()
+                counter_distance = 0
+                
+        
         
         # If the ball is in a position where the arm can take it,
         # we wait for a few iterations until we are sure that is the case
--- motor_control.py
+++ motor_control.py
@@ -8,13 +8,21 @@ half_right = half + 20
 half_left = half - 40
 range_f = 300
 
-def center_ball(x, pwm_1, pwm_2, m1, m2):
-    if x == -1:
+def center_ball(x, pwm_1, pwm_2, m1, m2, searching):
+    if x == -1 and not searching:
         GPIO.output(m1, True)
         GPIO.output(m2, False)
-        print("Ball not detected. Looking for ball.")
+        print("Ball not detected. Looking for ball. Rotating")
         pwm_1.ChangeDutyCycle(int(65))
         pwm_2.ChangeDutyCycle(int(65))
+        return -1
+    if x == -1 and searching:
+        GPIO.output(m1, False)
+        GPIO.output(m2, False)
+        print("Ball not detected. Looking for ball. Going forward")
+        pwm_1.ChangeDutyCycle(int(47))
+        pwm_2.ChangeDutyCycle(int(47))
+        return -1
     if x > half_left and x < half_right:
         print("On center")
         return 1
@@ -23,15 +31,15 @@ def center_ball(x, pwm_1, pwm_2, m1, m2):
         GPIO.output(m2, True)
         speed = int((x/range_f - 1)*15 + 50)
         print("Turning right")
-        pwm_1.ChangeDutyCycle(int(45))
-        pwm_2.ChangeDutyCycle(int(45))
+        pwm_1.ChangeDutyCycle(int(55))
+        pwm_2.ChangeDutyCycle(int(55))
         
     elif x < half_left:
         GPIO.output(m1, True)
         GPIO.output(m2, False)
         print("Turning left")
-        pwm_1.ChangeDutyCycle(int(45))
-        pwm_2.ChangeDutyCycle(int(45))
+        pwm_1.ChangeDutyCycle(int(60))
+        pwm_2.ChangeDutyCycle(int(60))
         
     return 0
 
@@ -39,9 +47,9 @@ def go_forward(pwm_1, pwm_2, m1, m2, y):
     GPIO.output(m1, False)
     GPIO.output(m2, False)
     if y > 300:
-        speed = 35
-    else:
         speed = 45
+    else:
+        speed = 50
     pwm_1.ChangeDutyCycle(speed)
     pwm_2.ChangeDutyCycle(speed)
     print("Going forward")
